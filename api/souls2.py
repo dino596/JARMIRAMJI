4459c42e419b5ac305e0d0ac3cc7a78f0ecc86a4
M8tth3 <mw.mountaindragon@gmail.com>
2023-10-15 18:49:26 -0700

    api surgery to transfer new api

--- api/souls2.py
+++ api/souls2.py
@@ -1,28 +1,89 @@
-from flask import Flask, Blueprint, request, jsonify
-from flask_restful import Api, Resource # used for REST API building
+from flask import Blueprint, request
+from flask_restful import Api, Resource, reqparse
+from __init__ import db
+from model.souls2 import Souls
 
-from model.souls import Data
+souls_bp = Blueprint("souls", __name__)
+souls_api = Api(souls_bp)
 
+class SoulsAPI(Resource):
+    def get(self):
+        id = request.args.get("id")
+        souls = db.session.query(Souls).get(id)
+        if souls:
+            return souls.to_dict()
+        return {"message": "not found"}, 404
 
-souls2_api = Blueprint('souls2_api', __name__, url_prefix='/api/souls')
-api = Api(souls2_api)
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("class_name", required=True, type=str)
+        parser.add_argument("health", required=True, type=int)
+        parser.add_argument("attack", required=True, type=int)
+        parser.add_argument("resistance", required=True, type=int)
+        parser.add_argument("power", required=True, type=int)
+        args = parser.parse_args()
+        souls = Souls(args["class_name"], args["health"], args["attack"], args["resistance"], args["power"])
 
-class DataResource(Resource):
-    def get(self, object_id):
         try:
-            data_object = Data.get_by_id(object_id)
-            
-            if data_object:
-                serialized_data = data_object.to_dict()
-                
-                return jsonify(serialized_data), 200
-            else:
-                return jsonify({'error': "object not found"}), 404
-        except Exception as e:
-            return jsonify({'error': "object not found"}), 500
-api.add_resource(DataResource, '/<int:object_id>')
+            db.session.add(souls)
+            db.session.commit()
+            return souls.to_dict(), 201
+        except Exception as exception:
+            db.session.rollback()
+            return {"message":f"error {exception}"}, 500
 
-if __name__ == '__main__':
-    app = Flask(__name__)
-    app.register_blueprints(souls_api)
-    app.run(debug=True)
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", required=True, type=int)
+        parser.add_argument("class_name", type=str)
+        parser.add_argument("health", type=int)
+        parser.add_argument("attack", type=int)
+        parser.add_argument("resistance", type=int)
+        parser.add_argument("power", type=int)
+        args = parser.parse_args()
+        
+        try:
+            souls = db.session.query(Souls).get(args["id"])
+            if souls:
+                if args["class_name"] is not None:
+                    souls.class_name = args["class_name"]
+                if args["health"] is not None:
+                    souls.health = args["health"]
+                if args["attack"] is not None:
+                    souls.attack = args["attack"]
+                if args["resistance"] is not None:
+                    souls.resistance = args["resistance"]
+                if args["power"] is not None:
+                    souls.power = args["power"]
+                db.session.commit()
+                return souls.to_dict(), 200
+            else:
+                return {"message": "not found"}, 404
+        except Exception as exception:
+            db.session.rollback()
+            return {"message": f"error {exception}"}, 500
+    
+    def delete(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id", required=True, type=int)
+        args = parser.parse_args()
+
+        try:
+            souls = db.session.query(Souls).get(args["id"])
+            if souls:
+                db.session.delete(souls)
+                db.session.commit()
+                return souls.to_dict()
+            else:
+                return {"message": "not found"}, 404
+        except Exception as exception:
+            db.session.rollback()
+            return {"message": f"error {exception}"}, 500
+
+class SoulsListAPI(Resource):
+    def get(self):
+        souls = db.session.query(Souls).all()
+        return [soul.to_dict() for soul in souls]
+
+souls_api.add_resource(SoulsAPI, "/souls")
+souls_api.add_resource(SoulsListAPI, "/soulsList")
--- main.py
+++ main.py
@@ -9,14 +9,14 @@ from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.players import initPlayers
-
+from model.souls2 import init_souls
 
 # setup APIs
 from api.covid import covid_api # Blueprint import api definition
 from api.joke import joke_api # Blueprint import api definition
 from api.user import user_api # Blueprint import api definition
 from api.player import player_api
-from api.souls import souls_api
+from api.souls2 import souls_bp
 
 from projects.projects import app_projects # Blueprint directory import projects definition
 
@@ -29,7 +29,7 @@ app.register_blueprint(covid_api) # register api routes
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
-app.register_blueprint(souls_api)
+app.register_blueprint(souls_bp)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -49,6 +49,7 @@ def activate_job():  # activate these items
     initJokes()
     initUsers()
     initPlayers()
+    init_souls()
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
